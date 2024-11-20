3e491ea613f4aa39b7aaad711827d7a25efe6917
Rafal Labunski <rafallabunski@gmail.com>
2024-11-20 21:20:04 +0100

    Fix event listener for filters and remove unnecessary console error

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,44 @@
 const booksList = document.querySelector('.books-list');
 const templateBook = Handlebars.compile(document.querySelector('#template-book').innerHTML);
+const filters = [];
 
 function render() {
+  booksList.innerHTML = '';
+
   for (const book of dataSource.books) {
-    const generatedHTML = templateBook(book);
-    const bookElement = utils.createDOMFromHTML(generatedHTML);
-    booksList.appendChild(bookElement);
+    const shouldBeHidden =
+      (filters.includes('adults') && !book.details.adults) ||
+      (filters.includes('nonFiction') && !book.details.nonFiction);
+
+    if (!shouldBeHidden) {
+      const generatedHTML = templateBook(book);
+      const bookElement = utils.createDOMFromHTML(generatedHTML);
+      booksList.appendChild(bookElement);
+    }
   }
 }
 
+function initActions() {
+  const filtersContainer = document.querySelector('.filters');
+
+  filtersContainer.addEventListener('click', function (event) {
+    const clickedElement = event.target;
+    if (
+      event.target.tagName === 'INPUT' &&
+            event.target.type === 'checkbox' &&
+            event.target.name === 'filter'
+    ) {
+      const filterValue = clickedElement.value;
+
+      if (clickedElement.checked && !filters.includes(filterValue)) {
+        filters.push(filterValue);
+      } else if (!clickedElement.checked && filters.includes(filterValue)) {
+        const index = filters.indexOf(filterValue);
+        filters.splice(index, 1);
+      }
+      console.log('Current Filters:', filters);
+    }
+  });
+}
 render();
+initActions();
